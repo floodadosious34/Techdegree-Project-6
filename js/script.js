3c672c6b438f6ce39b76cee1dbaa402dcc79b949
James Flood <floodadosious111@gmail.com>
2021-09-25 01:16:26 -0400

    added search bar feature

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@
 /* This function takes a list and a page number,then it iterates through list
    adding the html snippets to the page.
 */
+
 function showPage(list, page) {
    const startIndex = (page * 9) - 9;
    const endIndex = page * 9;
    const studentList = document.querySelector('.student-list');
    studentList.innerHTML = '';
-   for (i = 0; i < list.length; i++) {
+   for ( let i = 0; i < list.length; i++) {
       if (i >= startIndex && i < endIndex) {
          const studentItem = `
             <li class="student-item cf">
@@ -38,16 +39,20 @@ function addPagination (list) {
    const numOfPages = Math.ceil(list.length/9);
    const linkList = document.querySelector('.link-list');
    linkList.innerHTML = '';
-   for (i=1; i < numOfPages; i++) {
-      const button = `
-      <li>
-         <button type="button">${i}</button>
-      </li>
-      `;
-      linkList.insertAdjacentHTML('beforeend', button);
-   }
-   // Gets first button and sets it's class to active.
-   document.querySelector('.pagination button').className = 'active';
+      for (let i=1; i <= numOfPages; i++) {
+         let button = `
+            <li>
+               <button type="button">${[i]}</button>
+            </li>
+            `;
+         // Only displays button if there are more than 1 page needed
+         if (numOfPages >= 2) {
+            linkList.insertAdjacentHTML('beforeend', button);
+            // Gets first button and sets it's class to active.
+            document.querySelector('.pagination button').className = 'active';
+         }
+      }
+   
    
    // Event Listener for the buttons
    linkList.addEventListener('click', (e) => {
@@ -60,7 +65,61 @@ function addPagination (list) {
 };
 
 
+// Search bar Feature
+const header = document.querySelector('.header');
+const searchBar = `
+   <label for="search" class="student-search">
+      <span>Search by name</span>
+      <input id="search" placeholder="Search by name...">
+      <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+   </label>
+`;
+
+header.insertAdjacentHTML('beforeend', searchBar);
+
+//search function
+const search = document.querySelector('#search');
+//no matches variables
+document.querySelector('.header').insertAdjacentHTML('afterend',`<div id="noresults"><h2>No results found.</h2><p>Try again.</p></div>`);
+document.querySelector('#noresults').style.display = 'none';
+function searchName (input) {
+   //set input to lowercase 
+   let newInput = input.toString().toLowerCase();
+   //create new list 
+   let newList = [];
+   //loop through data and find matches
+   //if matches first or last name, push that student to the new students list
+   for (let i = 0; i < data.length ; i++) {
+      let firstName = data[i].name.first.toLowerCase();
+      let lastName = data[i].name.last.toLowerCase();
+      if (firstName.indexOf(newInput) !== -1) {
+         newList.push(data[i]);
+      } else if (lastName.indexOf(newInput) !== -1) {
+         newList.push(data[i]);
+      } 
+   }
+   
+   if (newList.length < 1) {
+      document.querySelector('#noresults').style.display = 'block';
+   } else {document.querySelector('#noresults').style.display = 'none';}
+   // function calls for search input
+   addPagination(newList);
+   showPage(newList,1);
+};
+
+//event listener for keyup on search bar
+search.addEventListener('keyup', (e) => {
+  let name = e.target.value;
+  searchName(name);
+});
+
+//event listener on submit button click
+const searchButton = search.nextElementSibling;
+searchButton.addEventListener('click', () => {
+      let name = search.value;
+      searchName(name);
+});
+
 // Call functions
 showPage(data,1 );
 addPagination(data);
-
